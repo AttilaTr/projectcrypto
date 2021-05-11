a7045301df5a44a2b805e05b28db10df21be944d
AttilaTr <attila.trekovanicz@gmail.com>
2021-05-11 17:53:12 +0000

    .

--- application/models.py
+++ application/models.py
@@ -3,7 +3,7 @@ from application import db
 class Crypto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    acronym = db.Column(db.String(15), nullable=False)
+    acronym = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(100), nullable=False)
     valueusd = db.Column(db.Integer, nullable=False)
     articles = db.relationship('Articles', backref='crypto')
--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,35 @@
 from application import app, db
+from application.models import Crypto
+from application.models import Articles
 from flask import render_template
 
-@app.route('/home/<word>')
-def home(word):
-    if word == 'Crypto':
-        return 'This will be the Crypto db'
-    if word == 'Articles':
-        return 'This will be the Article db'
-    else:
-        return 'Have a good day'
+@app.route('/add')
+def add():
+    new_crypto = Crypto(name = 'Name of crypto', acronym = 'Acronym of crypto', description = 'Descryption of crypto', valueusd = 'Value of crypto in USD')
+    db.session.add(new_crypto)
+    db.session.commit()
+    return 'New cryptocurrency added to database'
 
+@app.route('/read')
+def read():
+    all_crypto = Crypto.query.all()
+    crypto_string = ''
+    for crypto in all_crypto:
+        crypto_string += '<br>'+ crypto.name
+    return crypto_string
+
+@app.route('/update/<name>, <acronym>, <description>, <valueusd>')
+def update(name, acronym, description, valueusd):
+    first_crypto = Crypto.query.first()
+    first_crypto.name = name
+    first_crypto.acronym = acronym
+    first_crypto.name = description
+    first_crypto.name = valueusd
+    db.session.commit()
+    return first_crypto.name
+
+@app.route('/delete/')
+def delete():
+    crypto_to_delete = Crypto.query.first()
+    db.session.delete(crypto_to_delete)
+    db.session.commit()
