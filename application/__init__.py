7a7830820d38ec890771874404df5f3ecebff574
Attila Trekovanicz <82601849+AttilaTr@users.noreply.github.com>
2021-05-16 15:08:52 +0100

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@34.105.204.236/projectcrypto'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
